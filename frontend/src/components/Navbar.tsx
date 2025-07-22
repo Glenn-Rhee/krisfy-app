ff1665b8161580013d57af6e2ddb9bfb74f1a0d1
Glenn-Rhee <glennviktor5@gmail.com>
2025-07-22 20:22:34 +0700

    feat: added popover navbar each menues navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 // import { useWalletStore } from "@/store/useWalletStore";
 import Container from "./Container";
 import Button from "./Button";
+import Popover from "./Popover";
 
 export default function Navbar() {
   // const { connect } = useConnectWallet();
@@ -29,9 +30,143 @@ export default function Navbar() {
 
         {/* Menu */}
         <ul className="flex items-center gap-x-10 text-white text-sm">
-          <li className="hover:underline cursor-pointer">Products</li>
-          <li className="hover:underline cursor-pointer">Resources</li>
-          <li className="hover:underline cursor-pointer">Developers</li>
+          <li className="cursor-pointer flex items-center relative">
+            <Popover
+              triggerElement={
+                <>
+                  <span>Products</span>{" "}
+                  <i className="ri-arrow-drop-down-line text-xl"></i>
+                </>
+              }
+            >
+              <h6 className="text-[#50A1FE] sticky top-0 font-semibold">
+                Our Products
+              </h6>
+              <div className="mt-4 grid grid-cols-1 gap-y-3">
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <span>Krisfy</span>
+                  <p className="font-light">Core DeFi lending platform</p>
+                </div>
+                <div className="text-white overflow-hidden relative px-3 py-4 bg-gray-600/20 rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <span>
+                    Advanced Features{" "}
+                    <span className="bg-purple-800 py-1 px-3 rounded-xl text-xs font-light border border-gray-200/20">
+                      Coming Soon
+                    </span>
+                  </span>
+                  <p className="font-light">Core DeFi lending platform</p>
+                  <div className="absolute inset-0 bg-[#162030a8] w-full h-full" />
+                </div>
+              </div>
+            </Popover>
+          </li>
+          <li className="cursor-pointer flex items-center relative">
+            <Popover
+              triggerElement={
+                <>
+                  <span>Resources</span>{" "}
+                  <i className="ri-arrow-drop-down-line text-xl"></i>
+                </>
+              }
+            >
+              <h6 className="text-[#B473EF] sticky top-0 font-semibold">
+                Resources
+              </h6>
+              <div className="mt-4 grid grid-cols-1 gap-y-3">
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-global-line text-[#83CBFF] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Ecosystems</span>
+                      <p className="font-light">
+                        Explore our partners networks
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-government-fill text-[#9B9B9B] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Governance</span>
+                      <p className="font-light">
+                        Participate in protocol decisions
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-chat-1-fill text-white text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Help & Support</span>
+                      <p className="font-light">Get assistance and guidance</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-question-mark text-[#F8312F] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>FAQ</span>
+                      <p className="font-light">Frequently asked questions</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-blogger-fill text-[#F3EEF8] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Blog</span>
+                      <p className="font-light">Latest news and updates</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Popover>
+          </li>
+          <li className="cursor-pointer flex items-center relative">
+            <Popover
+              triggerElement={
+                <>
+                  <span>Developers</span>{" "}
+                  <i className="ri-arrow-drop-down-line text-xl"></i>
+                </>
+              }
+            >
+              <h6 className="text-[#06D16E] sticky top-0 font-semibold">
+                Developer Resources
+              </h6>
+              <div className="mt-4 grid grid-cols-1 gap-y-3">
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-stack-fill text-[#00A6ED] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Documentation</span>
+                      <p className="font-light">
+                        Comprehensive API and protocol docs
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-white px-3 py-4 bg-[#233044] rounded-xl border border-gray-200/20 font-semibold flex flex-col gap-y-1.5">
+                  <div className="flex items-center gap-x-2">
+                    <i className="ri-lock-line text-[#F9C23C] text-xl"></i>
+                    <div className="flex flex-col">
+                      <span>Security</span>
+                      <p className="font-light">
+                        Audit reports and security practices
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Popover>
+          </li>
         </ul>
 
         {/*  */}
